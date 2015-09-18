40972ee3bcb8cbf7d617effef46c782957e9545f
Gabe Hamilton <gabehamilton@gmail.com>
2015-09-18 11:32:43 -0600

    Base project for pair programming exercises, Queue files.

---
+++ test/unit/MyQueueTest.groovy
@@ -0,0 +1,18 @@
+/**
+ */
+class MyQueueTest extends GroovyTestCase {
+
+	MyQueue queue
+
+	void setUp() {
+		queue = new MyQueue()
+	}
+
+	void tearDown() {
+	}
+
+	void testSomething() {
+		fail('no tests yet')
+	}
+
+}
